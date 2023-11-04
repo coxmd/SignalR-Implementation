9831ae2fdc1d35ee417eae7d7883946eb86a3200
coxmd <coxmusyoki@gmail.com>
2023-11-04 12:49:51 +0300

    Created SignalR Hub and Added Methods to Hub

---
+++ SignalRSample2/Hubs/UserHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample2.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+
+            // send update to all clients that total views have been updated
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+    }
+}
