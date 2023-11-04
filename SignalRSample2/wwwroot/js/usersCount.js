e987ff8c3a9fe67d7d3fe74901c552819c977e7b
coxmd <coxmusyoki@gmail.com>
2023-11-04 13:20:17 +0300

    Call SignalR Hub method and recieved updates from hub implemented

---
+++ SignalRSample2/wwwroot/js/usersCount.js
@@ -0,0 +1,27 @@
+﻿//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("hubs/userCount").build();
+
+//connect to methods that hub invokes aka recieve notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+//start connection
+function fulfilled() {
+    //do something on start
+    console.log("Connection to User Hub Successful");
+}
+function rejected() {
+    //rejected logs
+    console.log("Connection to User Hub failed");
+}
+
+
+connectionUserCount.start().then(fulfilled, rejected);
